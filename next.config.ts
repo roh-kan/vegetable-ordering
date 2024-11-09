d1952e2566acc6a7872fe431a8266dc7868c53b6
roh-kan <rockingengineer02@gmail.com>
2024-11-09 13:00:29 +0530

    feat: update config for gh deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Enables static exports
+  basePath: process.env.NODE_ENV === 'production' ? '/vegetable-ordering' : '',
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
